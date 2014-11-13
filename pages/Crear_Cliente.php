3ba059d41e8eeeb915d4db72943665b4c8e853ee
josseluis <jose.luis.guevara@correounivalle.edu.co>
2014-11-13 17:51:10 -0500

    atributos_clientes
    tamaño de campos agregado

--- pages/Crear_Cliente.php
+++ pages/Crear_Cliente.php
@@ -19,20 +19,20 @@
                     <div class='form-group' >
                         <label  class='col-lg-3 control-label'>Nombre</label>
                         <div class='col-lg-9'>
-                            <input type='text' name='nom'  placeholder='Nombre' required='required' class='form-control' />
+                            <input type='text' name='nom'  placeholder='Nombre' required='required' class='form-control' maxlength=30/>
                         </div>
                     </div>
                     <div class='form-group' >
                         <label  class='col-lg-3 control-label'>Apellidos:</label>
                         <div class='col-lg-9'>
-                            <input type='text' name='apell'  placeholder='Apellidos' required='required' class='form-control'/>
+                            <input type='text' name='apell'  placeholder='Apellidos' required='required' class='form-control'maxlength=30/>
                         </div>
                     </div>
 
                     <div class='form-group' >
                         <label  class='col-lg-3 control-label'>Correo Electrónico:</label>
                         <div class='col-lg-9'>        
-                            <input type='text' name='e_mail'  placeholder='Correo Electr&oacute;nico' required='required' class='form-control' maxlength=30/>
+                            <input type='text' name='e_mail'  placeholder='Correo Electr&oacute;nico' required='required' class='form-control' maxlength=60/>
                         </div>
                     </div>
 
@@ -46,7 +46,7 @@
                     <div class='form-group' >
                         <label  class='col-lg-3 control-label'>Documento:</label>
                         <div class='col-lg-9'>
-                            <input type='text' name='doc'  placeholder='Documento' required='required' class='form-control' maxlength=30/>
+                            <input type='text' name='doc'  placeholder='Documento' required='required' class='form-control' maxlength=15/>
                         </div>
                     </div>
 
