6fcf5aa9fba67872269107453f7efdc122d2563c
josseluis <jose.luis.guevara@correounivalle.edu.co>
2014-11-29 02:10:56 -0500

    cliente
    max y min

--- modelos/Modelo_Cliente.php
+++ modelos/Modelo_Cliente.php
@@ -28,11 +28,11 @@ class Modelo_Cliente{
 		$valida = new Validacion_Datos(); // <- Para validar los tipos de datos
 		// Validacion de los minimos
 		if(!(strlen($this->cliente->get_Documento()) > 7))		$salida = 5;
-		elseif(!(strlen($this->cliente->get_Nombres()) > 2))	$salida = 2;
-		elseif(!(strlen($this->cliente->get_Apellidos()) > 2))	$salida = 3;
-		elseif(!(strlen($this->cliente->get_Direccion()) > 2))	$salida = 6;
+		elseif(!(strlen($this->cliente->get_Nombres()) > 1))	$salida = 2;
+		elseif(!(strlen($this->cliente->get_Apellidos()) > 1))	$salida = 3;
+		elseif(!(strlen($this->cliente->get_Direccion()) > 1))	$salida = 6;
 		elseif(!(strlen($this->cliente->get_Celular()) > 7))	$salida = 7;
-		elseif(!(strlen($this->cliente->get_Email()) > 6))		$salida = 4;
+		elseif(!(strlen($this->cliente->get_Email()) > 5))		$salida = 4;
 		// Validacion de los tipos de datos (Numérico,Alfabético,Alfanumérico)
 		elseif(!($valida->is_Number($this->cliente->get_Documento())))			$salida = 8;
 		elseif(!($valida->is_Alphabetic($this->cliente->get_Nombres())))		$salida = 9;
